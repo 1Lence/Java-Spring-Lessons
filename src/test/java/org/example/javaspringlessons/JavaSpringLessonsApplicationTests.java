053a5a3229d419867cf26689dabaf29cfa73aca5
1Lence <kirillpetrov173@gmail.com>
2025-02-19 14:18:48 +0500

    Create project

---
+++ src/test/java/org/example/javaspringlessons/JavaSpringLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaspringlessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLessonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
