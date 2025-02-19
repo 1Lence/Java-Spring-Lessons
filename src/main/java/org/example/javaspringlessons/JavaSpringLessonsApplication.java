94f19e0addedc5e1d5b1be932035cfe7edfe74db
1Lence <kirillpetrov173@gmail.com>
2025-02-19 16:08:21 +0500

    task 2

--- src/main/java/org/example/javaspringlessons/JavaSpringLessonsApplication.java
+++ src/main/java/org/example/javaspringlessons/JavaSpringLessonsApplication.java
@@ -3,8 +3,6 @@ package org.example.javaspringlessons;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
-import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class JavaSpringLessonsApplication {
